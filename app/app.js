cdc8b7d873feffbce2706fd0bf7d2b133853f97d
baikangwang <baikangwang@hotmail.com>
2017-03-23 23:05:19 +0800

    Completed the sample

--- app/app.js
+++ app/app.js
@@ -9,18 +9,59 @@ var server = http.createServer(function(request, response) {
             case "/index.html":
             case "/":
             case "index":
+                var html="<html>";
+                html+="<title>Hello World</title>";
+                html+="<body>";
+                html+="<h1>Hello World</h1>";
+                html+"<ul>";
+                html+="<li><a href='add.html'>Add</a></li>";
+                html+="<li><a href='edit.html'>Edit</a></li>";
+                html+="<li><a href='find.html'>Find</a></li>";
+                html+="<li><a href='delete.html'>Delete</a></li>";
+                html+="</ul>";
+                html+="</body>";
+                html+="</html>";
+                response.write(html);
                 break;
 
             case "/add.html":
+                var html="<html>";
+                html+="<title>Hello World</title>";
+                html+="<body>";
+                html+="Add";
+                html+="</body>";
+                html+="</html>";
+                response.write(html);
                 break;
 
             case "/edit.html":
+                var html="<html>";
+                html+="<title>Hello World</title>";
+                html+="<body>";
+                html+="Edit";
+                html+="</body>";
+                html+="</html>";
+                response.write(html);
                 break;
 
             case "/delete.html":
+                var html="<html>";
+                html+="<title>Hello World</title>";
+                html+="<body>";
+                html+="Delete";
+                html+="</body>";
+                html+="</html>";
+                response.write(html);
                 break;
 
             case "/find.html":
+                var html="<html>";
+                html+="<title>Hello World</title>";
+                html+="<body>";
+                html+="Find";
+                html+="</body>";
+                html+="</html>";
+                response.write(html);
                 break;
 
             default:
@@ -32,4 +73,4 @@ var server = http.createServer(function(request, response) {
     response.end();
 });
 
-server.listen("7798");
+server.listen("7790");
