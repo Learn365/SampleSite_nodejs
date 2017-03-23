8c4a7a8a47e33a6810441e02d493f11408ae10d1
baikangwang <baikangwang@hotmail.com>
2017-03-23 23:30:43 +0800

    fix javascript format

--- app/app.js
+++ app/app.js
@@ -5,62 +5,63 @@ var server = http.createServer(function(request, response) {
     console.log(request.url);
 
     if (request.method === "GET") {
+        var html = "";
         switch (request.url) {
             case "/index.html":
             case "/":
             case "index":
-                var html="<html>";
-                html+="<title>Hello World</title>";
-                html+="<body>";
-                html+="<h1>Hello World</h1>";
-                html+"<ul>";
-                html+="<li><a href='add.html'>Add</a></li>";
-                html+="<li><a href='edit.html'>Edit</a></li>";
-                html+="<li><a href='find.html'>Find</a></li>";
-                html+="<li><a href='delete.html'>Delete</a></li>";
-                html+="</ul>";
-                html+="</body>";
-                html+="</html>";
+                html += "<html>";
+                html += "<title>Hello World</title>";
+                html += "<body>";
+                html += "<h1>Hello World</h1>";
+                html += "<ul>";
+                html += "<li><a href='add.html'>Add</a></li>";
+                html += "<li><a href='edit.html'>Edit</a></li>";
+                html += "<li><a href='find.html'>Find</a></li>";
+                html += "<li><a href='delete.html'>Delete</a></li>";
+                html += "</ul>";
+                html += "</body>";
+                html += "</html>";
                 response.write(html);
                 break;
 
             case "/add.html":
-                var html="<html>";
-                html+="<title>Hello World</title>";
-                html+="<body>";
-                html+="Add";
-                html+="</body>";
-                html+="</html>";
+                html += "<html>";
+                html += "<title>Hello World</title>";
+                html += "<body>";
+                html += "Add";
+                html += "</body>";
+                html += "</html>";
                 response.write(html);
                 break;
 
             case "/edit.html":
-                var html="<html>";
-                html+="<title>Hello World</title>";
-                html+="<body>";
-                html+="Edit";
-                html+="</body>";
-                html+="</html>";
+                html += "<html>";
+                html += "<title>Hello World</title>";
+                html += "<body>";
+                html += "Edit";
+                html += "</body>";
+                html += "</html>";
                 response.write(html);
                 break;
 
             case "/delete.html":
-                var html="<html>";
-                html+="<title>Hello World</title>";
-                html+="<body>";
-                html+="Delete";
-                html+="</body>";
-                html+="</html>";
+                html += "<html>";
+                html += "<title>Hello World</title>";
+                html += "<body>";
+                html += "Delete";
+                html += "</body>";
+                html += "</html>";
                 response.write(html);
                 break;
 
             case "/find.html":
-                var html="<html>";
-                html+="<title>Hello World</title>";
-                html+="<body>";
-                html+="Find";
-                html+="</body>";
-                html+="</html>";
+                html += "<html>";
+                html += "<title>Hello World</title>";
+                html += "<body>";
+                html += "Find";
+                html += "</body>";
+                html += "</html>";
                 response.write(html);
                 break;
 
